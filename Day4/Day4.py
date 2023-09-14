fc0a4303b6b58194d51b8e1a1b9e4274823fe018
Meenakshi <snowpepper1203@gmail.com>
2023-09-14 23:03:28 +0530

    Add files via upload

---
+++ Day4/Day4.py
@@ -0,0 +1,12 @@
+print("=== Your Adventure Simulator ===")
+print("""You'll be asked a bunch of questions 
+then we'll make you up an amazing 
+story with YOU as the star! 🤩""")
+print()
+name = input("Your name: ")
+villain = input("Your worst enemy's name: ")
+superPower = input("Your super power: ")
+print()
+print("Our story begins as our hero","\033[36m" , name , "\033[0m","approaches a foreboding castle...")
+print("\033[0m","Suddenly a bolt of lightning striked the ground at the feet of","\033[36m", name)
+print("\033[0m"," 'Our final battle begins!' shouts the evil ","\033[31m", villain, "\033[0m""clealry missing the fact that","\033[36m", name, "\033[0m","has the power of","\033[32m", superPower, "\033[0m","which means they'll win quite easily")
