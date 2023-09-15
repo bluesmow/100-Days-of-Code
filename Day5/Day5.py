1de96b9ebf0aae57dbabad2b5190d4ebcf884139
Meenakshi <snowpepper1203@gmail.com>
2023-09-15 23:12:03 +0530

    Add files via upload

---
+++ Day5/Day5.py
@@ -0,0 +1,21 @@
+print("MarvelMovie Character Creator")
+print("--")
+print()
+myhero = input("Do you like 'hanging around'? :")
+myvoice = input("Do you have a 'gravely voice?'")
+marvel_feel = input("Do you often feel 'Marvelous'?:")
+
+if myhero == "Yes":
+  print("Then you're Spider-man")
+else :
+  print("Then you're Spiderman")
+
+if myvoice == "Yes" :
+  print("Then you're Korg")
+else :
+  print("Aww, then you're not Korg")
+
+if marvel_feel == "Yes" : 
+  print("Aha! You're Captain Marvel! Hi!")
+else : 
+  print("You're not Marvel")
