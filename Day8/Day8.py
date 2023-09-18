bbce388c65a779b200e9c4727c971344214e05e5
Meenakshi <snowpepper1203@gmail.com>
2023-09-18 18:39:22 +0530

    Add files via upload

---
+++ Day8/Day8.py
@@ -0,0 +1,16 @@
+print("Mean Old Insult Machine")
+print()
+print()
+name = input("Who are you?")
+if name == "Gojo" or name == "gojo":
+
+  
+  print()
+  hair = input("How much hair do you have on your head?")
+  print()
+  scalerate = input("On a scale of 1 to 10 , Rate yourself? (1: 🌭 , 10 : 👀) ")
+  print()
+
+
+
+print("Hey",name,"-yeah thought I wouldn't know who you were because you didn't capitalise properly! Well get that egg-head and dry your eyes because some amazing nsults are coming..")
